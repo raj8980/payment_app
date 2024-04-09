2e875465b9ef1b42b159b584b99969e5f31ea14a
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-04-09 18:03:41 +0930

    Implemented a logic to authenticate, search and update user details.

--- backend_express_mongo/models/User.js
+++ backend_express_mongo/models/User.js
@@ -1,18 +1,28 @@
 const zod = require("zod");
 
 const user = zod.object({
-    userName : zod.string().email().array().min(3).max(30),
-    firstName : zod.string().array().nonempty().length(50),
-    lastName : zod.string().array().nonempty().length(50),
-    password : zod.string().array().nonempty().min(6)
+    userName : zod.string().email().min(3).max(30),
+    firstName : zod.string().length(50),
+    lastName : zod.string().length(50),
+    password : zod.string().min(6)
 });
 
 const userSignIn = zod.object({
-    userName : zod.string().email().array().min(3).max(30),
-    password : zod.string().array().nonempty().min(6)
+    userName : zod.string().email().min(3).max(30),
+    password : zod.string().min(6)
 });
+
+const userUpdate = zod.object({
+     password : zod.string().min(6).optional(),
+     firstName : zod.string().length(50).optional(),
+     lastName : zod.string().length(50).optional
+});
+
+const filterParam = zod.string().length(50);
 
 module.exports={
     user,
-    userSignIn
-};
+    userSignIn,
+    userUpdate,
+    filterParam
+}; 
--- backend_express_mongo/routes/user.js
+++ backend_express_mongo/routes/user.js
@@ -3,9 +3,10 @@ const express = require("express");
 const router = express.Router();
 const jsonwebtoken = require("jsonwebtoken");
 
-const { user,userSignIn } = require("../models/User");
+const { user, userSignIn, userUpdate, filterParam } = require("../models/User");
 const { User } = require("../db");
 const { JWT_SECRET } = require("../config");
+const { authMiddleware } = require("../middleware");
 
 router.post("/signup", async (req, res) => {
   const userSignUpPayload = req.body;
@@ -32,7 +33,7 @@ router.post("/signup", async (req, res) => {
       });
 
       const userId = user._id;
-      const userJWT = jsonwebtoken.sign({ userId : userId }, JWT_SECRET);
+      const userJWT = jsonwebtoken.sign({ userId: userId }, JWT_SECRET);
 
       return res.status(200).json({
         message: "User created successfully",
@@ -42,30 +43,76 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/signin", async (req, res) => {
+  const userSignInPayload = req.body;
+  const { success } = userSignIn.safeParse(userSignInPayload);
 
-router.post("/signin", async (req,res) =>{
-    const userSignInPayload = req.body;
-    const { success } = userSignIn.safeParse(userSignInPayload);
+  if (!success) {
+    return res.status(411).json({
+      message: "Invalid user login payload",
+    });
+  }
 
-    if(!success){
-        return res.status(411).json({
-            message : "Invalid user login payload"
-        })
-    }
+  const user = await User.findOne({
+    userName: userSignInPayload.userName,
+    password: userSignInPayload.password,
+  });
+  if (!user) {
+    return res.status(411).json({
+      message: "Invalid user email id or password",
+    });
+  } else {
+    const userId = user._id;
+    const userJWT = jsonwebtoken.sign({ userId: userId }, JWT_SECRET);
 
-    const user = await User.findOne({userName: userSignInPayload.userName, password:userSignInPayload.password});
-    if(!user){
-        return res.status(411).json({
-            message : "Invalid user email id or password"
-        })     
-    }else {
-        const userId = user._id;
-        const userJWT = jsonwebtoken.sign({userId : userId},JWT_SECRET);
+    return res.status(200).json({
+      token: userJWT,
+    });
+  }
+});
 
-        return res.status(200).json({
-            token : userJWT 
-        });
-    }
+router.put("/", authMiddleware, async (req, res) => {
+  const updatePayload = req.body;
+  const { success } = userUpdate.safeParse(updatePayload);
+
+  if (!success) {
+    return res.status(411).json({
+      message: "User has passed invalid firstName or lastName or password",
+    });
+  }
+
+  await User.findOneAndUpdate(updatePayload, {
+    _id: updatePayload.userId,
+  });
+
+  return res.status(200).json({
+    message: "Updated successfully",
+  });
+});
+
+router.get("/bulk", authMiddleware, async (req, res) => {
+  const filter = req.query.filter || "";
+  const { success } = filterParam.safeParse(filter);
+  if (!success) {
+    return res.status(411).json({
+      message: "Invalid filter param",
+    });
+  }
+
+  const users = await User.find({
+    $or: [
+      { firstName: { $regex: `^${filter}`, $options: "i" } },
+      { lastName: { $regex: `^${filter}`, $options: "i" } },
+    ],
+  });
+  return res.status(200).json({
+    user: users.map((user) => ({
+      userName: user.userName,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
 });
 
 module.exports = router;
