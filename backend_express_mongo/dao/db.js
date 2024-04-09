5758b15a455879c46534e48a9652e35700f9532b
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-04-09 14:23:55 +0930

    Initial implementation of mongo and express in payment back end application.

---
+++ backend_express_mongo/dao/db.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const connectionURL = "mongodb+srv://admin:admin@cluster0.4hytupj.mongodb.net/payment";
+
+async function connect(){
+    await mongoose.connect(connectionURL);
+}
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName : {
+        type : String,
+        required : true,
+        trim : true,
+        maxLength : 50
+    },
+    lastName : {
+        type : String,
+        required : true,
+        trim : true,
+        maxLength : 50
+    },
+    password : {
+        type : String,
+        required : true,
+        minLength : 6
+    },
+    userName : {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true,
+        lowercase : true,
+        minLength : 3,
+        maxLength : 30
+    }
+});
+
+const User = mongoose.model("User",userSchema);
+
+module.exports ={
+    User
+};
