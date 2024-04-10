8495e6404fb584de440339ac5cf3580c3baf71c4
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-04-10 13:39:02 +0930

    Implemented basic initial step for payment frontend.

--- frontend_react_tailwind/src/App.jsx
+++ frontend_react_tailwind/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { SignUp } from "./pages/Signup";
+import { Dashboard } from "./pages/dashboard";
+import { TransferMoney } from "./pages/TransferMoney";
+import { SignIn } from "./pages/Signin";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" Component={SignUp}></Route>
+        <Route path="/signin" Component={SignIn}></Route>
+        <Route path="/send" Component={TransferMoney}></Route>
+        <Route path="/dashboard" Component={Dashboard}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
