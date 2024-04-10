788ea16392ff50df35b02c0c8387244ec53d7968
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-04-10 15:43:46 +0930

    Implemented a front end ui for sign up page with tailwind css. and connected to backend server.

--- frontend_react_tailwind/src/App.jsx
+++ frontend_react_tailwind/src/App.jsx
@@ -3,12 +3,13 @@ import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SignUp } from "./pages/Signup";
-import { Dashboard } from "./pages/dashboard";
 import { TransferMoney } from "./pages/TransferMoney";
-import { SignIn } from "./pages/Signin";
+import { SignIn } from "./pages/Signin"
+import { Dashboard } from "./pages/Dashboard";
 
 function App() {
   return (
+    <>
     <BrowserRouter>
       <Routes>
         <Route path="/signup" Component={SignUp}></Route>
@@ -17,6 +18,7 @@ function App() {
         <Route path="/dashboard" Component={Dashboard}></Route>
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
