41a05bc41bf6c065c522218025e0ffbdab5c8961
Raj Patel <38714169+raj8980@users.noreply.github.com>
2024-04-20 01:25:06 +0930

    Updated port

--- backend_express_mongo/index.js
+++ backend_express_mongo/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const rootRouter = require("./routes/index");
 
-const PORT = 3000;
+const PORT = 8080;
 
 const app = express();
 
